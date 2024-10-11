37feae840c58956ae31d0edf1c7938e1d6e8b4cc
eduardo <eeduardomnardone@gmail.com>
2024-10-11 18:50:59 +0200

    First commit

---
+++ first-install/single/useful-apts-personal.sh
@@ -0,0 +1,4 @@
+snap install discord  -y
+snap install spotify  -y
+snap install notion-snap-reborn  -y
+snap install whatsapp-for-linux  -y
---
+++ first-install/single/useful-apts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt install build-essential -y
+sudo apt install vim  -y
+sudo apt install gh  -y
+sudo apt -f install gnome-terminal  -y
+sudo apt install zsh  -y
+sudo apt install arandr  -y
+sudo apt install curl  -y
+sudo apt install blueman -y
+
+sudo snap install code --classic
+sudo snap install nvim --classic

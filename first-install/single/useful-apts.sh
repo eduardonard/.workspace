e27ea0c68298b1c41981eadd5f459ca64b566d3a
eduardonard <eduardomnardone@gmail.com>
2024-12-27 10:28:30 +0100

    a

--- first-install/single/useful-apts.sh
+++ first-install/single/useful-apts.sh
@@ -6,6 +6,7 @@ sudo apt -f install gnome-terminal  -y
 sudo apt install zsh  -y
 sudo apt install arandr  -y
 sudo apt install curl  -y
+sudo apt install blueman -y
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && sudo apt update && sudo apt install -y google-chrome-stable
 
 sudo snap install code --classic
